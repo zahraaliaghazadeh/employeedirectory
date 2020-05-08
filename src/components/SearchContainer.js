fa2b6e88633251b70bc6c2c6230f353c7ee49107
Yalda Ali Aghazadeh <zahraaliaghazadeh@Yaldas-MacBook-Pro.local>
2020-05-08 00:31:09 -0700

    added class searchcontainer component, added componentdidmount and inputchange and render

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,24 +1,99 @@
-import React, { Component } from "react"
-import API from "../utils/Api"
-import Container from "./Container"
+// initial test of the api 
+
+// import React, { Component } from "react"
+// import API from "../utils/Api"
+// import Container from "./Container"
+
+// class SearchContainer extends Component {
+//     state = {
+//         result: [],
+//         search: ""
+//     }
+
+//     componentDidMount() {
+//         this.apiSearch()
+//     }
+
+//     apiSearch = () => {
+//         API.search()
+//             .then(res => this.setState({ result: res.data.results }))
+//             .catch(err => console.log(err));
+
+//     }
+//     render(){
+//         return (<Container></Container>)
+//     }
+// }
+
+
+  
+import React, { Component } from "react";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+import Card from "./Card";
+import SearchForm from "./SearchForm";
+import EmpDetail from "./EmpDetail";
+import API from "../utils/API";
 
 class SearchContainer extends Component {
-    state = {
-        result: [],
-        search: ""
-    }
+  state = {
+    result: [],
+    search: ""
+  };
 
-    componentDidMount() {
-        this.apiSearch()
-    }
+  componentDidMount() {
+    this.searchEmp();
+  }
 
-    apiSearch = () => {
-        API.search()
-            .then(res => this.setState({ result: res.data.results }))
-            .catch(err => console.log(err));
+  searchEmp = () => {
+    API.search()
+      .then(res => this.setState({ result: res.data.results}))
+      .catch(err => console.log(err));
+  };
 
-    }
-    render(){
-        return (<Container></Container>)
-    }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+   
+  };
+
+  
+
+  render() {
+    return (
+      <Container>
+        <Row>
+        <Col size="md-12">
+            <Card heading="Search">
+              <SearchForm
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          
+          <Col size="md-12">
+            <Card
+              heading={"Employees:"}
+            >
+              {/* to sort the table */}
+              {[...this.state.result].filter(item =>{return item.name.first.toLowerCase().indexOf( this.state.search)>-1}).map((res, index)=><div > <EmpDetail key={index} name={res.name.first +" "+ res.name.last} src={res.picture.thumbnail} director={res.email} age={res.dob.age} phone={res.phone}/> <hr/></div>)}
+            
+            </Card>
+          </Col>
+          
+        </Row>
+      </Container>
+    );
+  }
 }
+
+export default SearchContainer;
